af2e5fc4dc6ea6e282ac04288c65b7e602e3bb49
LeeBhin <bini4039@naver.com>
2024-03-15 20:37:36 +0900

    mobile test

--- src/js/chatpage.js
+++ src/js/chatpage.js
@@ -191,6 +191,7 @@ function drawMap(w, g) {
 
         // 지도를 표시할 div와 지도 옵션으로 지도를 생성합니다
         var map = new kakao.maps.Map(mapContainer, mapOption);
+        container.style.zIndex = 1;
 
         // 로드뷰 객체를 생성합니다 
         var roadview = new kakao.maps.Roadview(rvContainer);
