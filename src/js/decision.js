c8cc342da67511e85f5dbab3892095ca3bdf21b9
LeeBhin <bini4039@naver.com>
2024-03-16 13:39:34 +0900

    almost done

--- src/js/decision.js
+++ src/js/decision.js
@@ -83,7 +83,6 @@ async function Answer(Question) {
                     "!설명서"를 통해 사용법을 확인하실 수 있습니다.`;
                 }
                 const schoolList = SchList(Question)
-                console.log(SchList(Question))
                 return schoolList.List;
             }
             else if (ifDuplicate) {
